439204c8561b2e919e42d43afdb56382dc648234
Victor Hsieh <victor@csie.org>
2013-01-23 17:23:13 +0800

    exec bit

--- fetch.sh
+++
@@ -1,7 +0,0 @@
-law=criminal
-record='http://lis.ly.gov.tw/lgcgi/lglaw?@40:1804289383:f:NO%3DB04536*%20AND%20NO%3DA2%24%246$$$PD'
-byline='http://lis.ly.gov.tw/lgcgi/lglaw?@40:1804289383:f:NO%3DE04536*%20OR%20NO%3DB04536$$10$$$NO-PD'
-bytime='http://lis.ly.gov.tw/lgcgi/lglaw?@40:1804289383:f:NO%3DE04536*%20OR%20NO%3DB04536$$11$$$PD%2BNO'
-
-curl $bytime | iconv -f big5 -c > data/$law-bytime.html
-curl $record | iconv -f big5 -c > data/$law-record.html
