65b850d9edf1f67a86428d6695edf8e05f1a9306
ChangZhuo Chen <czchen@gmail.com>
2013-03-23 20:51:45 +0800

    Create data directory before fetching

--- prepare_categories.sh
+++ prepare_categories.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PORTAL='http://lis.ly.gov.tw/lgcgi/lglaw?@98:1804289383:g:CN%3D0100*%20AND%20NO%3DA1%24%241__'
+mkdir -p data
 curl $PORTAL | iconv -f big5 -c > data/index.html
 
 grep '<TD class=cl1>' data/index.html | sed -e 's/.*cl1>//' |awk '{print "data/law/"$1"/index.html"}' > data/file.txt
